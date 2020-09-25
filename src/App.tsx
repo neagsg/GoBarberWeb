64f0bd643a16116eb1c0e45dedfd46a283d6439f
neagsg <geansg4@gmail.com>
2020-09-25 00:00:19 -0300

    Conectando a API e fazendo autenticação com ContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
       <GlobalStyle />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </>
   );
 };
